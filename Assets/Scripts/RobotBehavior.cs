9639a14470f10386af7c883763785acddad774c9
george <70286749+GeorgeEllis123@users.noreply.github.com>
2025-02-20 13:50:27 -0500

    Added robot to scene and updated behavior

--- Assets/Scripts/RobotBehavior.cs
+++ Assets/Scripts/RobotBehavior.cs
@@ -5,6 +5,7 @@ public class RobotBehavior : MonoBehaviour
     public float speed = 3f;
     public float distance = 10f;
     public float detectionWindow = 2f;
+    public AudioSource audioSource;
 
     private Vector3 startPos;
     private bool movingForward = true;
@@ -28,6 +29,7 @@ public class RobotBehavior : MonoBehaviour
             if (Vector3.Distance(startPos, transform.position) >= distance)
             {
                 movingForward = false;
+                transform.Rotate(0,180,0);
             }
         }
         else
@@ -36,6 +38,7 @@ public class RobotBehavior : MonoBehaviour
             if (Vector3.Distance(startPos, transform.position) <= 0.1f)
             {
                 movingForward = true;
+                transform.Rotate(0, 180, 0);
             }
         }
     }
@@ -48,7 +51,11 @@ public class RobotBehavior : MonoBehaviour
 
         if (distFromStart >= lowerBound && distFromStart <= upperBound)
         {
-            Debug.Log("Detecting player");
+            Debug.Log("Can see player");
+        }
+        else
+        {
+            Debug.Log("Can't see player");
         }
     }
 }
