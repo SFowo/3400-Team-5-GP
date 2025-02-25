01c3d0979e402f71c16816bd03090418a615d49a
RayKong <ray.hyun.kong@gmail.com>
2025-02-25 17:41:40 -0500

    made some changes to submission + improve level manager

--- Assets/Scripts/Common/LevelManager.cs
+++ Assets/Scripts/Common/LevelManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,15 +9,13 @@ namespace Common
     {
         #region Inspector Variables
 
-    
-
         [Serializable]
         public class TriggerZone
         {
             public Collider collider;
             public TriggerType triggerType;
         }
-    
+
         public enum TriggerType
         {
             AirLock,
@@ -29,16 +28,26 @@ namespace Common
 
         [Header("Common")] 
         [SerializeField] private AudioSource audioSource;
-        [SerializeField] private GameObject player;
-    
-        [Header("AirLock")] 
-        [SerializeField] private AudioClip airLockSound;
-        [SerializeField] private AudioClip initialDialogue;
 
-        // Add more headers and objects
+        [Header("AirLock")] 
+        [SerializeField] private AudioClip airLockSound;   // Background noise (loops)
+        [SerializeField] private AudioClip initialDialogue; // One-time dialogue
+        [SerializeField] private GameObject keyPadScreen;
+        [SerializeField] private Animator airLockAnimator; //door animator
+
         #endregion
 
-        #region Trigger&Delgator
+        #region Unity Methods
+
+        private void Start()
+        {
+            StartCoroutine(PlayDialogueAndHandleAirlockSequence());
+        }
+
+        #endregion
+
+        #region Trigger & Delegator
+
         private void OnTriggerEnter(Collider other)
         {
             foreach (var zone in triggerZones)
@@ -71,29 +80,56 @@ namespace Common
             switch (type)
             {
                 case TriggerType.AirLock:
-                    audioSource.PlayOneShot(airLockSound);
-                    Invoke(nameof(OpenAirLock), airLockSound.length);
+                    StartCoroutine(PlayDialogueAndHandleAirlockSequence());
                     break;
 
                 case TriggerType.FirstFloor:
-
                     break;
 
                 case TriggerType.SecondFloor:
-
                     break;
 
                 case TriggerType.ThirdFloor:
-
                     break;
             }
         }
-    
+
         #endregion
+
+        #region Airlock Handling
+
+        private IEnumerator PlayDialogueAndHandleAirlockSequence()
+        {
+            // Step 1: Play both dialogue & background noise immediately
+            audioSource.PlayOneShot(initialDialogue);
+            audioSource.clip = airLockSound;
+            audioSource.loop = true;
+            audioSource.Play();
+            Debug.Log("Playing initial dialogue & looping airlock sound...");
+
+            // Step 2: Wait for dialogue to finish
+            yield return new WaitForSeconds(initialDialogue.length);
+
+            // Step 3: After 0.5 sec, switch keypad to green
+            yield return new WaitForSeconds(0.5f);
+            keyPadScreen.GetComponent<Renderer>().material.color = Color.green;
+            Debug.Log("Keypad turned green!");
+
+            // Step 4: After another 0.5 sec, open the airlock door
+            yield return new WaitForSeconds(0.5f);
+            OpenAirLock();
+            Debug.Log("Airlock door opened!");
+            audioSource.Stop();
+            audioSource.clip = null;
+        }
+        
 
         private void OpenAirLock()
         {
-        
+            airLockAnimator.SetBool("Open", true);
+            airLockAnimator.SetBool("Close", false);
         }
+
+        #endregion
     }
 }
